c3c61aaef24c795eba9e3f03498e34eb4dc355a8
Pankaj Baswal <pankajbaswal3360@gmail.com>
2025-08-14 12:07:40 +0530

    set basci setup for soical app

---
+++ Social-App/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+function App() {
+  
+  return (
+    <>
+      Social App
+      <Button variant="contained">Hello world</Button>;
+    </>
+  )
+}
+
+export default App
